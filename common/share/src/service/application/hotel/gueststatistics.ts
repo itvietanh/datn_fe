4df30b6c7bf86732c42d0c6548ffa105cf8da146
hieu1332004 <hieunvph32759@fpt.edu.vn>
2024-11-25 09:21:52 +0700

    thống kê khách hàng và nhân viên

---
+++ common/share/src/service/application/hotel/gueststatistics.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BaseService } from '../../base/base.service';
+import { PagedListModel, ResponseModel } from 'share';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Gueststatistics extends BaseService {
+  protected override prefix = 'statistic/guest';
+
+  public getDataStatisticalTrans<T = any>(params: any = null) {
+    return this.http.get<ResponseModel<T>>(`${this.baseUrl}`, {
+      params: this.stringifyParams(params)
+    });
+  }
+
+  public exportExcelTrans(params: any = null) {
+    return this.http.get(`${this.baseUrl}/export-guest`, {
+      params: this.stringifyParams(params),
+      responseType: 'blob'
+    });
+  }
+}
